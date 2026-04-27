b46ae53ffcb075f2fcee0f27ddd559f4a8826fdf
Yaakoub-S <yaakoubsaouti2001@gmail.com>
2026-04-28 01:13:24 +0200

    efi partition formatting

--- partition.sh
+++ partition.sh
@@ -9,14 +9,32 @@ error_out() {
   exit 1
 }
 
+EFI_MNTDIR=/mnt/efi
+ROOT_MNTDIR=/mnt
+
 # 1. Parsing drive name
 drive=$1
-[[ ! -e "/sys/block/$drive" ]] && error_out "'$1' is not a valid drive."
+name=${1##*/}
 
-# 2. Asking Permission before nuking everything
+[[ -z "$drive" || ! -e "/sys/block/$drive" ]] && error_out "'$1' is not a valid drive."
+
+# 2. Asking permission before nuking everything
 read -p "Do you want to wipe '$drive'? [y/N] " res
 [[ $res != 'y' ]] && exit 1
 
 # 3. Partitioning
-echo -e "label: gpt\n,1G,U\n," | sfdisk -fq --wipe always --wipe-partitions always "$drive" >/dev/null 2>&1
+echo -e "label: gpt\n,1G,U\n," | sfdisk -fq --wipe always --wipe-partitions always "$drive" &>/dev/null
 (($? != 0)) && error_out "failed creating the necessary partitions."
+
+# 4. Get partitions
+mapfile -ts 1 partitions < <(sfdisk "$drive" -lqo Device)
+efi_part="${partitions[0]}"
+root_part="${partitions[1]}"
+
+# 5. Format partitions
+# 5.1 Format the root partition
+# TODO
+# 5.2 Format the efi partition
+mkdir -p "$EFI_MNTDIR"
+mkfs.fat -F 32 "$efi_part"
+mount "$efi_part" "$EFI_MNTDIR"
