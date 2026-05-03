e8234f25849f386ed1a98aa8a1614953390fc3e3
Yaakoub-S <yaakoubsaouti2001@gmail.com>
2026-05-03 18:20:12 +0200

    add drive preparation steps

--- modules/01_partition.sh
+++ modules/01_partition.sh
@@ -11,7 +11,19 @@ name=${1##*/}
 
 [[ -z "$drive" || ! -e "/sys/block/$name" ]] && error_out "'$1' is not a valid drive."
 
-read -p "Do you want to wipe '$drive'? [y/N] " res
-[[ $res != 'y' ]] && exit 1
+# Preparing the drive for partitioning
+swapoff -a &>/dev/null
+umount -R /mnt &>/dev/null
 
+if [[ -b "/dev/mapper/cryptroot" ]]; then
+  sync
+  fuser -kvm /dev/mapper/cryptroot &>/dev/null
+  sleep 2
+  cryptsetup close cryptroot &>/dev/null
+fi
+
+read -p "Do you want to wipe '$drive'? [y/N] " res
+[[ $res ~= 'y|Y' ]] && exit 1
+
+# Partitioning
 echo -e "label: gpt\n,1G,U\n," | sfdisk -fq --wipe always --wipe-partitions always "$drive" || error_out "failed creating the necessary partitions."
